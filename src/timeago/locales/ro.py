1e359ca7ba2d6bc7767fea05ecf4553deeddfbfe
CaRaM3LL <caramelizatul@yahoo.com>
2020-11-22 21:19:32 +0200

    Add files via upload

---
+++ src/timeago/locales/ro.py
@@ -0,0 +1,21 @@
+'''
+Created on 2016-5-26
+@author: Husse
+'''
+
+LOCALE = [
+    ["acum", "acum un timp"],
+    ["acum %s secunde", "în %s secunde"],
+    ["acum 1 minut", "într-un minut"],
+    ["acum %s minute", "în %s minute"],
+    ["acum o oră", "într-o oră"],
+    ["acum %s", "în %s ore"],
+    ["acum o zi", "într-o zi"],
+    ["acum %s zile", "în %s zile"],
+    ["acum o săptămână", "într-o săptămână"],
+    ["acum %s săptămâni", "în %s săptămâni"],
+    ["acum o lună", "într-o lună"],
+    ["acum %s luni", "în %s luni"],
+    ["acum un an", "într-un an"],
+    ["acum %s ani", "în %s ani"],
+]
